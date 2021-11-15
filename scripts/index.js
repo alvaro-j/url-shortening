814aeb27e53a4c1d05bbe0ad80d410fc96271975
alvaro-j <alvarojaime360@gmail.com>
2021-11-15 11:48:22 -0300

    add link on js and adjust word too long

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,8 @@ btnMobile.addEventListener("click", toggleMenu);
 let btnShorten = document.querySelector("#btn-shorten");
 let input = document.querySelector("#input-shorten-link");
 let error = document.querySelector("#error");
+let wrapper = document.querySelector("#purpleBg-wrapper");
+
 const handleShortenLink = async () => {
 	if (input.value === "") {
 		console.log("input vazio");
@@ -20,6 +22,15 @@ const handleShortenLink = async () => {
 		const res = await fetch(url);
 		const data = await res.json();
 		console.log(data);
+		const link = `
+			<div class="div-link">
+			<span>${input.value}</span>
+			<div>
+				<span>${data.result.short_link}</span>
+				<button>Copy</button>
+			</div>
+		</div>`;
+		wrapper.innerHTML += link;
 	}
 };
 btnShorten.addEventListener("click", handleShortenLink);
